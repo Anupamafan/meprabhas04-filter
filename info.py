567384880a407f63a766b5c24f95d42e8398130e
Prabhas <118895531+Anupamafan@users.noreply.github.com>
2025-01-18 23:08:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -65,7 +65,7 @@ PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b>- ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʟ�
 OWNER_USERNAME = environ.get('OWNER_USERNAME', 'iam_channel_admin') # owner username without @
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+1QPOjABsKX43MTM1')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+1Ka-0wRWAAM1ZmMx')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Movies_Telugu_Top')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_shortLinks/20')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/How_To_Open_shortLinks/20')
