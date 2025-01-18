f6bae0ef3f0ebf3cca398e7915bd67de1d8df59e
Prabhas <118895531+Anupamafan@users.noreply.github.com>
2025-01-18 23:09:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -127,7 +127,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://nearby-geralda-kushal-kumar-4ce33b1a.koyeb.app/")
+URL = environ.get("URL", "https://public-dina-darling-prabhas-9ce6b73c.koyeb.app/")
 
 
 # Rename Info ; If True Then Bot Rename File Else Not
