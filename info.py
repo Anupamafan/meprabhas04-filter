3dd6eb838ad4f75cc23b14cdc5f53224d9a7a0be
Prabhas <118895531+Anupamafan@users.noreply.github.com>
2025-06-20 22:12:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+1QPOjABsKX43MTM1') # Support Chat L
 
 # True Or False
 PM_SEARCH = bool(environ.get('PM_SEARCH', True)) # In Pm Search Currently Spell Check Doesn't Work.
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False))
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
 VERIFY = bool(environ.get('VERIFY', False))
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
@@ -92,8 +92,8 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Shortlink Info
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'linkcents.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'e11fdc88060dffdaf4e0f18ed57336d6c94a7a68')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'bharatlinks.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '3fcae87f2a66502452397b37fcd62e6ca3f04ac2')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
